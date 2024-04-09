4da13cb05df30764b28ec1eb05c936fad107cf87
saikiran2946 <132703795+saikiran2946@users.noreply.github.com>
2024-04-10 00:07:02 +0530

    Add files via upload

--- matrices.java
+++ matrices.java
@@ -69,6 +69,23 @@ public class matrices {
             }
         }
     }
+    public static void boundaryTraversal(int[][]a){
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[0][i]+" ");
+        }
+        System.out.println();
+        for(int i=1;i<a.length;i++){
+            System.out.print(a[i][a[i].length-1]+" ");
+        }
+        System.out.println();
+        for(int i=a[0].length-2;i>=0;i--){
+            System.out.print(a[a.length-1][i]+" ");
+        }
+        System.out.println();
+        for(int i=a.length-1;i>=1;i--){
+            System.out.print(a[i][0]+" ");
+        }
+    }
     
     public static void main(String[] args) {
         int[][]a={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
@@ -84,6 +101,7 @@ public class matrices {
         //printmatrix(a);
         //spiralMatrix(a);
         //snakePattern(a);
+        boundaryTraversal(a);
     }
     
 }
